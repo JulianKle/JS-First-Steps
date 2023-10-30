1c5b92648e3a0697d1207a017595ecf03201f4c5
Julian Klett <julian22a@googlemail.com>
2023-10-30 09:32:02 +0100

    for zählschleife

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,14 @@
-function showMessage() {
-  alert("Hallo Welt");
+const coordinates = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+  [7, 8],
+  [9, 10],
+];
+
+for (let i = 0; i < coordinates.length; i++) {
+  const coordinate = coordinates[i];
+  for (let j = 0; j < coordinate.length; j++) {
+    console.log(coordinate[j]);
+  }
 }
-showMessage();
